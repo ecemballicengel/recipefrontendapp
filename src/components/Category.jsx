b5b0579eac3405e57b4d41fc7dc392bbbd097367
Ecem Ballı Cengel <141059963+ecemballicengel@users.noreply.github.com>
2024-02-06 22:47:37 +0300

    Kategoriler eklendi

---
+++ src/components/Category.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import getData from "../services/GetService";
+function Category() {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const fetchedCategories = await getData("Category");
+        setCategories(fetchedCategories);
+      } catch (error) {
+        console.error("Kategoriler alinamadi:", error);
+      }
+    };
+    fetchCategories();
+  }, []);
+  return (
+    <div className="container text-center">
+     <ul className="list-group list-group-horizontal">
+      {categories.slice(0,5).map((category) => (
+      <a href="/category/">
+      <div className="card text-bg-dark" style={{ margin:"20px"}}>
+       <img src="https://picsum.photos/200/200" className="card-img" alt="..." />
+       <div className="card-img-overlay">
+         <h5 className="card-title">{category.name}</h5>
+       </div>
+     </div>     
+      </a>
+      ))}
+      </ul>
+      <ul className="list-group list-group-horizontal">
+      {categories.slice(5).map((category) => (
+      <a href="/category/">
+      <div className="card text-bg-dark" style={{ margin:"20px"}}>
+       <img src="https://picsum.photos/200/200" className="card-img" alt="..." />
+       <div className="card-img-overlay">
+         <h5 className="card-title">{category.name}</h5>
+       </div>
+     </div>     
+      </a>
+      ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Category;
